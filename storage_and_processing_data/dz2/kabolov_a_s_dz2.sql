2b6913fe90990b63b94e624151006c4e5047a342
Alexander <alexander@kabolov.com>
2025-02-25 19:14:03 +0100

    typo2

--- storage_and_processing_data/dz2/kabolov_a_s_dz2.sql
+++ storage_and_processing_data/dz2/kabolov_a_s_dz2.sql
@@ -1,17 +1,17 @@
 select distinct brand from transaction t where t.standard_cost > 1500;
 
-select * from transaction t where t.order_status = 'Approved' and to_date(t.transaction_date, 'DD.MM.YYYY') between '2017-04-01' and '2017-04-09'
+select * from transaction t where t.order_status = 'Approved' and to_date(t.transaction_date, 'DD.MM.YYYY') between '2017-04-01' and '2017-04-09';
 
 select * from customer c where (c.job_industry_category IN ('IT', 'Financial Services')) and c.job_title like 'Senior%';
 
 select brand 
 from transaction t
-left join customer c on t.customer_id = c.customer_id
+join customer c on t.customer_id = c.customer_id
 where c.job_industry_category = 'Financial Services';
 
 select distinct c.*
 from customer c
-left join transaction t on t.customer_id = c.customer_id
+join transaction t on t.customer_id = c.customer_id
 where t.brand in ('Giant Bicycles', 'Norco Bicycles', 'Trek Bicycles')
 limit 10;
 
